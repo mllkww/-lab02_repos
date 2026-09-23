ab615966eb72f8a021d87b6efd5450d5feb94892
Міла Кепіч <liudmyla.kepich.ri.2026@lpnu.ua>
2026-09-23 22:27:38 +0300

    first master commit

---
+++ PR2.0.cpp
@@ -0,0 +1,26 @@
+﻿// Lab_02.cpp
+// < Кепіч Людмила >
+// Лабораторна робота № 2.
+// Лінійні програми
+// Варіант 13
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main()
+{
+	double alpha; // вхідний параметр
+	double beta; // вхідний параметр
+	double z1; // результат обчислення 1-го виразу
+	double z2; // результат обчислення 2-го виразу
+	cout << "alpha = "; cin >> alpha;
+	cout << "beta = "; cin >> beta;
+	z1 = (sin(alpha) + cos(2 * beta - alpha)) /
+		(cos(alpha) - sin(2 * beta - alpha));
+	z2 = (1 + sin(2 * beta)) / cos(2 * beta);
+	cout << endl;
+	cout << "z1 = " << z1 << endl;
+	cout << "z2 = " << z2 << endl;
+	cin.get();
+	return 0;
+}
+
