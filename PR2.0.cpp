c25c31d9b92d075fd1296afe0e7a73cd3c8a9cca
Міла Кепіч <liudmyla.kepich.ri.2026@lpnu.ua>
2026-09-24 00:16:47 +0300

    change 1

--- PR2.0.cpp
+++ PR2.0.cpp
@@ -10,16 +10,16 @@ int main()
 {
 	double alpha; // вхідний параметр
 	double beta; // вхідний параметр
-	// double z1; // результат обчислення 1-го виразу
+	 double z1; // результат обчислення 1-го виразу
 	 double z2; // результат обчислення 2-го виразу
 	cout << "alpha = "; cin >> alpha;
 	cout << "beta = "; cin >> beta;
-	// z1 = (sin(alpha) + cos(2 * beta - alpha)) /
+	 z1 = (sin(alpha) + cos(2 * beta - alpha)) /
 		(cos(alpha) - sin(2 * beta - alpha));
 	 z2 = (1 + sin(2 * beta)) / cos(2 * beta);
 	cout << endl;
-	//cout << "z1 = " << z1 << endl;
-	 cout << "z2 = " << z2 << endl;
+	cout << "z1 = " << z1 << endl;
+	cout << "z2 = " << z2 << endl;
 	cin.get();
 	return 0;
 }
